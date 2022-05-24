1e5653f412f6f3b1086c19463edaa4b225b8016a
jawad-nsu <jawadurrahim1997@gmail.com>
2022-05-24 21:00:40 +0600

    stripe frontend to backend call, stripe promise instance

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,14 +1,18 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req.body.cartItems);
     try {
       const params = {
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options: [{ shipping_rate: "" }, { shipping_rate: "" }],
+        shipping_options: [
+          { shipping_rate: "shr_1L2yfBFbDPeJAp5AmfiMsdPY" },
+          { shipping_rate: "shr_1L2yfsFbDPeJAp5AJJvYInIR" },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
