b4e802bf17e4c50f1bf36d261e0fe470ed7b1a0a
jawad-nsu <jawadurrahim1997@gmail.com>
2022-06-01 23:47:06 +0600

    fixed small bugs & completed project

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,7 +16,12 @@ import { Toaster } from "react-hot-toast";
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
   return (
     <div>
       <div className="product-detail-container">
@@ -61,9 +66,7 @@ const ProductDetails = ({ product, products }) => {
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
-                {qty}
-              </span>
+              <span className="num">{qty}</span>
               <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
@@ -78,7 +81,7 @@ const ProductDetails = ({ product, products }) => {
               Add to Cart
             </button>
             <Toaster />
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy now
             </button>
           </div>
