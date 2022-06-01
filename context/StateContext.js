834d02a08c9368adc6712809b03bd3aaaae6f43a
jawad-nsu <jawadurrahim1997@gmail.com>
2022-06-01 22:42:32 +0600

    working on integrating canvas confetti

--- context/StateContext.js
+++ context/StateContext.js
@@ -110,6 +110,9 @@ export const StateContext = ({ children }) => {
         onAdd,
         toggleCartItemQuantity,
         onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
--- pages/success.js
+++ pages/success.js
@@ -3,11 +3,16 @@ import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
 
 import { useStateContext } from "../context/StateContext";
+import { runFireworks } from "../lib/utils";
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
   useEffect(() => {
     localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
   }, []);
   return (
     <div className="success-wrapper">
